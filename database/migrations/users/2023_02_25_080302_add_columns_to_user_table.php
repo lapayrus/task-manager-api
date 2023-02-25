c9cb56cda4e24da56e2ab7b4a7cd60499060a1fb
= <rao.suryapal.23@gmail.com>
2023-02-25 13:58:28 +0530

    changes in the migration directory, and added new cols to users table

---
+++ database/migrations/users/2023_02_25_080302_add_columns_to_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 100);
+            $table->string('last_name', 100)
+                ->nullable();
+            $table->string('username')
+                ->unique();
+            $table->softDeletes();
+            
+            if (Schema::hasColumn('users', 'name')) {
+                $table->dropColumn('name');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'username']);
+            $table->dropSoftDeletes();
+
+            $table->string('name');
+        });
+    }
+};
